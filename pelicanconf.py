c7b109cd77e9a6deb8d2ee2082825a259c20c8a6
Stokori <stokoriel@gmail.com>
2017-01-02 03:14:23 -0600

    Add sitemap plugin
    I have no idea if I'll actually make any use of this but hey

--- pelicanconf.py
+++ pelicanconf.py
@@ -35,7 +35,7 @@ THEME = 'theme'
 FORMATTED_FIELDS = []
 
 PLUGIN_PATHS = ['./plugins']
-PLUGINS = ['assets']
+PLUGINS = ['assets','sitemap']
 
 DIRECT_TEMPLATES = ['index', 'categories', 'tags']
 STATIC_PATHS = [
@@ -57,7 +57,21 @@ CATEGORY_SAVE_AS = '{slug}/index.html'
 
 AUTHOR_SAVE_AS = ''
 
-MENUITEMS = [ ('Home',  SITEURL + '/'), ('Test', '#') ]
+MENUITEMS = [ ('Home',  SITEURL + '/') ]
+
+SITEMAP = {
+    'format': 'xml',
+    'priorities': {
+        'articles': 0.5,
+        'indexes': 0.5,
+        'pages': 0.5
+    },
+    'changefreqs': {
+        'articles': 'monthly',
+        'indexes': 'daily',
+        'pages': 'monthly'
+    }
+}
 
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
