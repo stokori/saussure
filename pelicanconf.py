53df2edf500a5911d0b65226724a92d128997e51
Stokori <stokoriel@gmail.com>
2017-01-02 02:34:40 -0600

    Add menu

--- pelicanconf.py
+++ pelicanconf.py
@@ -57,7 +57,7 @@ CATEGORY_SAVE_AS = '{slug}/index.html'
 
 AUTHOR_SAVE_AS = ''
 
-DISPLAY_CATEGORIES_ON_MENU = True
+MENUITEMS = [ ('Home',  SITEURL + '/'), ('Test', '#') ]
 
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
